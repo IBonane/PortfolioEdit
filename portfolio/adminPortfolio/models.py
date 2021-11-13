75715da2184010c2e3e8fe4e4210f824109d9c2d
Ibonane <bonanedjimbala@gmail.com>
2021-11-13 14:13:25 +0100

    second commit

--- portfolio/adminPortfolio/models.py
+++ portfolio/adminPortfolio/models.py
@@ -4,17 +4,21 @@ from django.db import models
 # Create your models here.
 
 class UserInfo(models.Model):
-    names = models.CharField(max_length=255, verbose_name="Nom et prenom", primary_key=True)
-    competenceDomain = models.CharField(max_length=400, verbose_name="Domaine de compétence")
-    mail = models.EmailField(verbose_name="EMail")
-    phone = models.CharField(max_length=20, verbose_name="numéro de téléphone")
-    image = models.ImageField(verbose_name="image")
+    names = models.CharField(max_length=255, verbose_name="Nom et prenom *", primary_key=True)
+    competenceDomain = models.CharField(max_length=400, verbose_name="Domaine de compétence *")
+    mail = models.EmailField(verbose_name="Adresse mail *")
+    phone = models.CharField(max_length=20, verbose_name="numéro de téléphone *")
+    imageProfile = models.ImageField(verbose_name="image de profile",  blank=True)
+    firstParagraph = models.TextField(verbose_name="Mettez une petite phrase d'accroche",  blank=True)
+    wordsOfFirstParagraph = models.TextField(verbose_name="Citez quelques outils ou logiciels que vous maitriser", blank=True)
+    imageParagraph = models.ImageField(verbose_name="image de la phrase d'accroche", blank=True)
 
     def __str__(self):
         return self.names
 
 
 class Competence(models.Model):
+    author = models.ForeignKey(UserInfo, verbose_name="Auteur", on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name="Titre de la compétence")
     year = models.DateField(verbose_name="Année")
     explain = models.TextField(verbose_name="Détails")
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+SECRET_KEY = 'django-insecure-0cd2(tc9vta+5^_^qdz2p$1!k%d48#6r5th+-o)w^&361r=l-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,9 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
